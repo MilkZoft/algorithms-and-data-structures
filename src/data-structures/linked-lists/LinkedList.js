e267f8afa2a25698703546a96ca8e0a303c94f15
Carlos Santana <csantana@beachbody.com>
2017-03-01 17:38:21 -0800

    Adding LinkedList

--- src/data-structures/linked-lists/LinkedList.js
+++ src/data-structures/linked-lists/LinkedList.js
@@ -1,23 +1,29 @@
-/* export default function LinkedList() {
-  // Helper function
-  function Node(element) {
-    this.next = element;
-    this.next = null;
+/* // Helper function
+function Node(element) {
+  this.element = element;
+  this.next = null;
+}
+
+export default class LinkedList {
+  constructor() {
+    this.listSize = 0;
+    this.head = null;
   }
 
-  let listSize = 0;
-  let head = null;
+  getHead() {
+    return this.head;
+  }
 
-  this.append = element => {
+  append(element) {
     let node = new Node(element);
     let current;
 
     // Si la lista esta vacía (Primer elemento)
-    if (head === null) {
-      head = node;
+    if (this.head === null) {
+      this.head = node;
     } else {
       // Si ya tiene datos comenzamos a recorrer la lista desde head
-      current = head;
+      current = this.head;
 
       // Mientras exista un elemento en el nodo next... (cuando llegue a null sera el final de la lista)
       while(current.next) {
@@ -30,39 +36,113 @@
     }
 
     // Incrementamos el tamaño de la lista
-    listSize++;
-  };
+    this.listSize++;
+  }
 
- this.insert = (position, element) => {
+  insert(position, element) {
+    // Validamos el rango
+    if (position >= 0 && position <= this.listSize) {
+      let node = new Node(element);
+      let current = this.head;
+      let previous;
+      let index = 0;
 
- };
+      // Agregando un elemento al inicio de la lista
+      if (position === 0) {
+        node.next = current;
+        this.head = node;
+      } else {
+        // Recorrer la lista hasta la posicion indicada
+        while (index++ < position) {
+          previous = current;
+          current = current.next;
+        }
 
- this.removeAt = position => {
+        node.next = current;
+        previous.next = node;
+      }
 
- };
+      // Incrementamos el tamaño de la lista
+      this.listSize++;
 
- this.remove = element => {
+      return true;
+    }
 
- };
+    return false;
+  }
 
- this.indexOf = element => {
+  removeAt(position) {
+    // Validamos que la posición este dentro del rango
+    if (position > -1 && position < this.listSize) {
+      let current = this.head;
+      let previous;
+      let index = 0;
 
- };
+      // Quitando el primer elemento...
+      if (position === 0) {
+        this.head = current.next;
+      } else {
+        // Recorremos la lista hasta la posicion indicada
+        while (index++ < position) {
+          previous = current;
+          current = current.next;
+        }
 
- this.hasElements = () => {
+        // Enlazamos el elemento anterior con el next de current (saltamos el elemento para eliminarlo)
+        previous.next = current.next;
+      }
 
- };
+      this.listSize--;
 
- this.size = () => {
+      // Retornamos el elemento eliminado...
+      return current.element;
+    }
 
- };
+    // Si la posicion esta fuera de rango, simplemente retornamos null
+    return null;
+  }
 
- this.toString = () => {
+  remove(element) {
+    let index = this.indexOf(element);
 
- };
+    return this.removeAt(index);
+  }
 
- this.print = () => {
+  indexOf(element) {
+    let current = this.head;
+    let index = 0;
 
- };
-}
-*/
+    while (current) {
+      if (current.element === element) {
+        return index;
+      }
+
+      index++;
+      current = current.next;
+    }
+
+    return -1;
+  }
+
+  hasElements() {
+    return this.listSize > 0;
+  }
+
+  size() {
+    return this.listSize;
+  }
+
+  print() {
+    let current = this.head;
+    let string = '';
+    let index = 0;
+
+    while (current) {
+      string += `[${index}]${current.element}${current.next ? '--->' : ''}`;
+      current = current.next;
+      index++;
+    }
+
+    return string;
+  }
+} */
--- test/globalTest.js
+++ test/globalTest.js
@@ -3,3 +3,11 @@ require('babel-register');
 
 // Global methods
 global.assert = require('chai').assert;
+
+// Console.log colors
+global.cyan = '\x1b[36m%s\x1b[0m';
+global.yellow = '\x1b[33m';
+global.blue = '\x1b[34m';
+global.green = '\x1b[32m';
+global.red = '\x1b[31m';
+global.magenta = '\x1b[35m';

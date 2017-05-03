6298d940d0fe80c8cc45c27480ebc1a999ed7d1a
Carlos Santana <csantana@beachbody.com>
2017-05-03 09:00:17 -0700

    Adding Selection Sort algorithm

--- src/algorithms/sorting/bubble/bubble.js
+++ src/algorithms/sorting/bubble/bubble.js
@@ -1,34 +1,18 @@
-const array = createArray(1000);
-let spins = 1;
-console.log('UNSORTED', array);
+import { createArray, swap } from '../../arrayUtils';
+
+let array = createArray(10);
+
+console.log('UNSORTED', array.toString(), '\n');
 
 bubleSort();
 
-console.log('SORTED', array);
-
-function createArray(size) {
-  const arr = [];
-
-  for (let i = size; i > 0; i--) {
-    arr.push(i);
-  }
-
-  return arr;
-}
-
-function swap(indexA, indexB) {
-  const tmp = array[indexA];
-  array[indexA] = array[indexB];
-  array[indexB] = tmp;
-
-  console.log(spins++, '-', indexA, indexB, 'SWAPPING===', tmp, array[indexA]);
-}
+console.log('\nSORTED', array.toString());
 
 function bubleSort() {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - 1; j++) {
       if (array[j] > array[j + 1]) {
-        swap(j, j + 1);
+        array = swap(j, j + 1, array);
       }
     }
   }
